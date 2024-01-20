208ff7f216f32bc61039fdf8d9839aafcf70d0f5
rohanmenon1 <menon67@Purdue.edu>
2024-01-20 01:55:10 -0500

    i aM a ProGRammRr!

--- cool-investment-game/src/App.jsx
+++ cool-investment-game/src/App.jsx
@@ -4,6 +4,7 @@ import Dashboard from './dashboard';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,12 +18,14 @@ const firebaseConfig = {
   storageBucket: "cool-investment-game.appspot.com",
   messagingSenderId: "409980403207",
   appId: "1:409980403207:web:c73dcd26fc024f7bbf34a2",
-  measurementId: "G-V9GP5E2N8Y"
+  measurementId: "G-V9GP5E2N8Y",
+  databaseURL: "https://cool-investment-game-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
 
 function App() {
 
