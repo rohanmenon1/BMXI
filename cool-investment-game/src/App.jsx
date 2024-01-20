d2b85a28250021fd48599dad613c43c315171a72
rohanmenon1 <menon67@Purdue.edu>
2024-01-20 00:59:27 -0500

    wOw fIrsT feAtuRe

--- cool-investment-game/src/App.jsx
+++ cool-investment-game/src/App.jsx
@@ -1,6 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
 import Dashboard from './dashboard';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCC3slWmSWZebwE1MMVAE8MCwKK5gYS4cI",
+  authDomain: "cool-investment-game.firebaseapp.com",
+  databaseURL: "https://cool-investment-game-default-rtdb.firebaseio.com",
+  projectId: "cool-investment-game",
+  storageBucket: "cool-investment-game.appspot.com",
+  messagingSenderId: "409980403207",
+  appId: "1:409980403207:web:c73dcd26fc024f7bbf34a2",
+  measurementId: "G-V9GP5E2N8Y"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
 
