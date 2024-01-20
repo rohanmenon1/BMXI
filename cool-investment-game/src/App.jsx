cefffcd6acc1f900792afc8958d71d4943638efe
Darshxx13 <81164868+Darshxx13@users.noreply.github.com>
2024-01-20 03:00:54 -0500

    Update App.jsx

--- cool-investment-game/src/App.jsx
+++ cool-investment-game/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Login</h1>
+        {!isLoggedIn && <h1>Login</h1>}
         {isLoggedIn ? (
           <Dashboard />
         ) : (
